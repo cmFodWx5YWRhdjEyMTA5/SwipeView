2b2f532e474b590600d51fabf3cc3285e8f286c1
umut47 <usoysal41@gmail.com>
2019-02-14 23:12:13 +0300

    example app
    library used

--- app/src/main/java/com/umut/soysal/swipeview/MainActivity.java
+++ app/src/main/java/com/umut/soysal/swipeview/MainActivity.java
@@ -2,14 +2,126 @@ package com.umut.soysal.swipeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.umut.soysal.swipelayoutview.helper.StackItemTouchHelperCallback;
+import com.umut.soysal.swipelayoutview.manager.StackLayoutViewManager;
+import com.umut.soysal.swipelayoutview.ui.OnSwipeListener;
+import com.umut.soysal.swipelayoutview.util.Utility;
 
-public class MainActivity extends AppCompatActivity
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Integer> list = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
     }
+
+    private void initView() {
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new MyAdapter());
+        StackItemTouchHelperCallback cardCallback = new StackItemTouchHelperCallback(recyclerView.getAdapter(), list);
+        cardCallback.setOnSwipedListener(new OnSwipeListener<Integer>() {
+
+            @Override
+            public void onSwiping(RecyclerView.ViewHolder viewHolder, float ratio, int direction) {
+                MyAdapter.MyViewHolder myHolder = (MyAdapter.MyViewHolder) viewHolder;
+                viewHolder.itemView.setAlpha(1 - Math.abs(ratio) * 0.2f);
+                if (direction == Utility.SWIPING_LEFT) {
+                    myHolder.dislikeImageView.setAlpha(Math.abs(ratio));
+                } else if (direction == Utility.SWIPING_RIGHT) {
+                    myHolder.likeImageView.setAlpha(Math.abs(ratio));
+                } else {
+                    myHolder.dislikeImageView.setAlpha(0f);
+                    myHolder.likeImageView.setAlpha(0f);
+                }
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, Integer o, int direction) {
+                MyAdapter.MyViewHolder myHolder = (MyAdapter.MyViewHolder) viewHolder;
+                viewHolder.itemView.setAlpha(1f);
+                myHolder.dislikeImageView.setAlpha(0f);
+                myHolder.likeImageView.setAlpha(0f);
+                Toast.makeText(MainActivity.this, direction == Utility.SWIPED_LEFT ? "like" : "no like", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipedClear() {
+                Toast.makeText(MainActivity.this, "data clear", Toast.LENGTH_SHORT).show();
+                recyclerView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        initData();
+                        recyclerView.getAdapter().notifyDataSetChanged();
+                    }
+                }, 3000L);
+            }
+
+        });
+        final ItemTouchHelper touchHelper = new ItemTouchHelper(cardCallback);
+        final StackLayoutViewManager cardLayoutManager = new StackLayoutViewManager(recyclerView, touchHelper);
+        recyclerView.setLayoutManager(cardLayoutManager);
+        touchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private void initData() {
+        list.add(R.drawable.img_avatar_01);
+        list.add(R.drawable.img_avatar_02);
+        list.add(R.drawable.img_avatar_03);
+        list.add(R.drawable.img_avatar_04);
+        list.add(R.drawable.img_avatar_05);
+        list.add(R.drawable.img_avatar_06);
+        list.add(R.drawable.img_avatar_07);
+
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            ImageView avatarImageView = ((MyViewHolder) holder).avatarImageView;
+            avatarImageView.setImageResource(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView avatarImageView;
+            ImageView likeImageView;
+            ImageView dislikeImageView;
+
+            MyViewHolder(View itemView) {
+                super(itemView);
+                avatarImageView = (ImageView) itemView.findViewById(R.id.iv_avatar);
+                likeImageView = (ImageView) itemView.findViewById(R.id.iv_like);
+                dislikeImageView = (ImageView) itemView.findViewById(R.id.iv_dislike);
+            }
+
+        }
+    }
+
 }
